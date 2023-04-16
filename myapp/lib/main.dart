be51bdd098bdc5caecd9121d8c61c72de8150aa2
Davidyang2149 <davidyang2149@gmail.com>
2023-04-16 22:52:56 +0900

    위젯 위치를 조정해봐라

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -8,26 +8,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First App',
+      title: 'Practice Card',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MyCard(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyCard extends StatelessWidget {
+  const MyCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First app'),
+        title: const Text('BBANTO'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        elevation: 0.0,
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
             Text('Hello'),
             Text('Hello'),
