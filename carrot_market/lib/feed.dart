3be885b52617e69fcb15d9cfce513a3f02f0ced5
Davidyang2149 <davidyang2149@gmail.com>
2023-07-16 12:16:31 +0900

    좋아요 기능을 구현하라

--- carrot_market/lib/feed.dart
+++ carrot_market/lib/feed.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Feed extends StatelessWidget {
+class Feed extends StatefulWidget {
   const Feed({
     super.key,
   });
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  bool isFavorite = false;
 
   @override
   Widget build(BuildContext context) {
@@ -57,12 +64,18 @@ class Feed extends StatelessWidget {
                 children: [
                   Spacer(),
                   GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      setState(() {
+                        isFavorite = !isFavorite; // 좋아요 토글
+                      });
+                    },
                     child: Row(
                       children: [
                         Icon(
-                          CupertinoIcons.heart,
-                          color: Colors.black54,
+                          isFavorite
+                              ? CupertinoIcons.heart_fill
+                              : CupertinoIcons.heart,
+                          color: isFavorite ? Colors.pink : Colors.black,
                           size: 16,
                         ),
                         Text(
